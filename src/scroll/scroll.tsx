929d874e064608c2bb4b06fc7dae1f0bb8fcb5d3
Sebastian Herrera <sebastian.herrera.concha@gmail.com>
2023-12-24 16:43:15 -0300

    cambios en el scroll

--- src/scroll/scroll.tsx
+++ src/scroll/scroll.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import solteclogo from '../assets/Imagenes/Soltec-logo.png';
+import solteclogo from '../assets/Imagenes/Solteclogo2.png';
+import scrolledLogo from '../assets/Imagenes/soltec-logo.png'; // Nueva imagen para el logo scroll
 
 const Scroll: React.FC = () => {
   const [scrolled, setScrolled] = useState(false);
@@ -20,7 +21,7 @@ const Scroll: React.FC = () => {
   return (
     <nav className={`navbar navbar-expand-lg navbar-light fixed-top py-3 ${scrolled ? 'scrolled' : ''}`} style={{ backgroundColor: scrolled ? '#FFFFFF' : 'transparent' }}>
       <div className="container px-4 px-lg-5">
-        <img className={`solteclogo ${scrolled ? 'scrolled' : ''}`} src={solteclogo} alt="Service" style={{ color: scrolled ? '#DC5834' : '#FFFFFF' }} />
+        <img className={`solteclogo ${scrolled ? 'scrolled' : ''}`} src={scrolled ? scrolledLogo : solteclogo} alt="Service" style={{ color: scrolled ? '#DC5834' : '#FFFFFF' }} />
 
         <button className="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse"
           data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
@@ -28,7 +29,7 @@ const Scroll: React.FC = () => {
         <div className={`collapse navbar-collapse ${scrolled ? 'show' : ''}`} id="navbarResponsive">
           <ul className={`navbar-nav ms-auto my-2 my-lg-0 ${scrolled ? '' : 'scrolled'}`} style={{ color: scrolled ? '#000000' : '#ffffff' }}>
             <li className="nav-item" style={{ marginRight: '20px' }}><a className="nav-link" href="#repuestos" style={{ color: scrolled ? '#000000' : '#ffffff' }}>Repuestos</a></li>
-            <li className="nav-item" style={{ marginRight: '20px' }}><a className="nav-link" href="#nuestrasmarcas" style={{ color: scrolled ? '#000000' : '#ffffff' }}>Nuestroas Marcas</a></li>
+            <li className="nav-item" style={{ marginRight: '20px' }}><a className="nav-link" href="#nuestrasmarcas" style={{ color: scrolled ? '#000000' : '#ffffff' }}>Nuestras Marcas</a></li>
             <li className="nav-item" style={{ marginRight: '20px' }}><a className="nav-link" href="#portfolio" style={{ color: scrolled ? '#000000' : '#ffffff' }}>Servicios</a></li>
             <li className="nav-item" style={{ marginRight: '20px' }}><a className="nav-link" href="#contact" style={{ color: scrolled ? '#000000' : '#ffffff' }}>Contactanos</a></li>
             <div className="d-flex justify-content-end">
